2c20280e04566433e5bb8f5129b2f226ab3d3d51
Mani <manesorapalli@gmail.com>
2023-10-03 16:24:16 +0530

    update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnets" {
   cidr_block = var.public_subnets[count.index]
   availability_zone = var.azs[count.index]
   tags = {
-    Name = "public-subnet-${count.index+1}"
+    Name = "public-subnet-${count.index + 1}"
   }
   }
 
@@ -21,7 +21,7 @@ resource "aws_subnet" "private_subnets" {
   cidr_block = var.private_subnets[count.index]
   availability_zone = var.azs[count.index]
   tags = {
-    Name = "private-subnet-${count.index+1}"
+    Name = "private-subnet-${count.index + 1}"
   }
 }
 
